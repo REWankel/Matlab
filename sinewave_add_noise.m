252a214d8c929c41227a2ba7550cbd356a0802b1
Winston Chang <winstonharvetech@gmail.com>
2025-08-01 14:23:50 +0800

    Create sinewave_add_noise.m

---
+++ sinewave_add_noise.m
@@ -0,0 +1,16 @@
+clear all
+close all
+clc
+home
+
+Sample_Point   = 1000;
+ADC_Resolution = 14;
+Amplitude      = 2^ADC_Resolution;
+Noise_Gain     = 3000;
+
+t = 0:2*pi/(Sample_Point-1):2*pi;                %Radius for one sinewave period
+sinewave = Amplitude*sin(10*t);                  %pure sinewave multiply with Amplitude
+random_noise = Noise_Gain*randn(1,Sample_Point); %1000 noise data multiply with Noise_Gain
+
+noise_sine = sinewave + random_noise;            %sinewave+random noise
+plot(noise_sine)
